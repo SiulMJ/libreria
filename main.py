3e8c10263a1ab4e4741a89c99b98552fa6b9397f
Siulmj <luis.esgrima02@gmail.com>
2024-12-22 17:21:54 -0600

    commit

--- main.py
+++ main.py
@@ -6,4 +6,8 @@ app = Flask(__name__, template_folder="template")
 def index():
     return render_template('index.html')
 
+@app.route('/catalogo')
+def catalogo():
+    return render_template('catalogo.html')
+
 app.run(host="localhost", port=4000, debug=True)
