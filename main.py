b88d04b204a3bc8b19e74371bfc791f48c9e8fb2
Luis Angel Mondragon Jimenez <luis.esgrima02@gmail.com>
2025-02-19 09:42:02 -0600

    update proyect

--- main.py
+++ main.py
@@ -14,8 +14,20 @@ def catalogo():
 def otros():
     return render_template('otros.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/registro')
+def registro():
+    return render_template('registro.html')
+
 @app.route('/mangas')
 def mangas():
     return render_template('mangas.html')
+
+@app.route('/libro')
+def libros():
+    return render_template('libro.html')
 
 app.run(host="localhost", port=4000, debug=True)
